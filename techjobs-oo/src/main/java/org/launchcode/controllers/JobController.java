cd5eae5742056a155513993c05f16fccb09d780c
Trev05 <tpearman@bigrivercom.com>
2017-04-10 16:43:35 -0500

    Add files via upload
    Final code

---
+++ techjobs-oo/src/main/java/org/launchcode/controllers/JobController.java
@@ -0,0 +1,53 @@
+package org.launchcode.controllers;
+
+
+
+import org.launchcode.models.Job;
+import org.launchcode.models.data.JobFieldData;
+import org.launchcode.models.forms.JobForm;
+import org.launchcode.models.data.JobData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+@RequestMapping(value = "job")
+public class JobController {
+
+    private JobData jobData = JobData.getInstance();
+
+    // The detail display for a given Job at URLs like /job?id=17
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model, int id) {
+
+        model.addAttribute(jobData.findById(id));
+        return "job-detail";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute(new JobForm());
+        return "new-job";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @Valid JobForm jobForm, Errors errors) {
+
+        if(errors.hasErrors()){
+            model.addAttribute(jobForm);
+            return "new-job";
+        }
+        Job job = new Job(jobForm.getName(), JobData.getInstance().getEmployers().findById(jobForm.getEmployerId()), JobData.getInstance().getLocations().findById(jobForm.getLocationId()), JobData.getInstance().getPositionTypes().findById(jobForm.getPositionTypeId()), JobData.getInstance().getCoreCompetencies().findById(jobForm.getCoreCompetencyId()));
+        JobData.getInstance().add(job);
+
+        return "redirect:?id=" + job.getId();
+
+    }
+}
---
+++ techjobs-oo/src/main/java/org/launchcode/models/forms/JobForm.java
@@ -0,0 +1,130 @@
+package org.launchcode.models.forms;
+
+import org.launchcode.models.CoreCompetency;
+import org.launchcode.models.Employer;
+import org.launchcode.models.Location;
+import org.launchcode.models.PositionType;
+import org.launchcode.models.data.JobData;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+
+/**
+ * Created by LaunchCode
+ */
+public class JobForm {
+
+    @NotNull
+    @Size(min=1, message = "Name may not be empty")
+    private String name;
+
+    @NotNull
+    private int employerId;
+
+    private int locationId;
+    private int coreCompetencyId;
+    private int positionTypeId;
+
+    @NotNull
+    @Size(min=1, message = "Employers may not be empty")
+    private ArrayList<Employer> employers;
+
+    @NotNull
+    @Size(min=1, message = "Location may not be empty")
+    private ArrayList<Location> locations;
+
+    @NotNull
+    @Size(min=1, message = "Core Competency may not be empty")
+    private ArrayList<CoreCompetency> coreCompetencies;
+
+    @NotNull
+    @Size(min=1, message = "Position may not be empty")
+    private ArrayList<PositionType> positionTypes;
+
+
+
+    public JobForm() {
+
+        JobData jobData = JobData.getInstance();
+
+        employers = jobData.getEmployers().findAll();
+        locations = jobData.getLocations().findAll();
+        coreCompetencies = jobData.getCoreCompetencies().findAll();
+        positionTypes = jobData.getPositionTypes().findAll();
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployerId() {
+        return employerId;
+    }
+
+    public void setEmployerId(int employerId) {
+        this.employerId = employerId;
+    }
+
+    public ArrayList<Employer> getEmployers() {
+        return employers;
+    }
+
+    public void setEmployers(ArrayList<Employer> employers) {
+        this.employers = employers;
+    }
+
+    public ArrayList<Location> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(ArrayList<Location> locations) {
+        this.locations = locations;
+    }
+
+    public ArrayList<CoreCompetency> getCoreCompetencies() {
+        return coreCompetencies;
+    }
+
+    public void setCoreCompetencies(ArrayList<CoreCompetency> coreCompetencies) {
+        this.coreCompetencies = coreCompetencies;
+    }
+
+    public ArrayList<PositionType> getPositionTypes() {
+        return positionTypes;
+    }
+
+    public void setPositionTypes(ArrayList<PositionType> positionTypes) {
+        this.positionTypes = positionTypes;
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public int getCoreCompetencyId() {
+        return coreCompetencyId;
+    }
+
+    public void setCoreCompetencyId(int coreCompetencyId) {
+        this.coreCompetencyId = coreCompetencyId;
+    }
+
+    public int getPositionTypeId() {
+        return positionTypeId;
+    }
+
+    public void setPositionTypeId(int positionTypeId) {
+        this.positionTypeId = positionTypeId;
+    }
+}
